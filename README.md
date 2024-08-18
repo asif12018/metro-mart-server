83f3f91505d0f3b27ac38229d371ca0c6841d93e
asif12018 <asif.request@gmail.com>
2024-08-18 19:27:37 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,46 @@
+
+# Project Title
+
+Metro-mart: this is a website where you can search product. filter by category, or sort it out by price range, date etc.
+
+
+
+
+## Features
+
+- Responsive website for all device
+- Live previews
+- Fullscreen mode
+- Cross platform
+- search or filter out the product
+- firebase to secure user data
+
+
+## Teachnolgy Used:
+- React
+- firebase
+- express
+- node.js
+- tanstack query
+- axios
+- react form hook
+
+
+
+
+
+
+## Demo
+
+live site: https://metro-mart.netlify.app/
+
+to install this or run it on locally:
+
+1. first you have to copy or download the file for both frontend and backend
+
+2. npm install
+
+
+
+
+
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_NAME}@cluster0.ecpul2e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://admin2:admin2@cluster0.ecpul2e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -82,10 +82,10 @@ async function run() {
 
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
